36c19372d04d81adb31f1fea4541848f6afd9be1
王腾 <wangteng@yingmob.com>
2018-04-25 19:44:28 +0800

    singer

---
+++ vue-music/src/api/singer.js
@@ -0,0 +1,18 @@
+import jsonp from 'common/js/jsonp'
+import {commonParams, options} from './config'
+
+export function getSingerList() {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg'
+  const data = Object.assign({}, commonParams, {
+    channel: 'singer',
+    page: 'list',
+    key: 'all_all_all',
+    pagesize: 100,
+    pagenum: 1,
+    loginUin: 853556568,
+    hostUin: 0,
+    platform: 'yqq',
+    needNewCode: 0
+  })
+  return jsonp(url, data, options)
+}
